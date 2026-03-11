0de84e455a9f26eee8beebbad5213ec0fb8ca2c9
BrandonARU <bh468@student.aru.ac.uk>
2026-03-11 12:06:28 +0000

    Website update, functioning js quiz import

--- Website/JS/quizImportProportionalReasoning.js
+++ Website/JS/quizImportProportionalReasoning.js
@@ -2,13 +2,126 @@ var storedJSON;
 var score = 0;
 var totalQuestionNo = 0;
 
-function fetchJson(loadingJSON){
+function loadLocalJSON(){
+    storedJSON = {
+    "Questions":[
+        {
+            "Question" : "Write 3/8 as a percentage.",
+            "Options" : ["12.5", "33.3", "37.5", "42.12"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Work out 15/32 as a decimal.",
+            "Options" : ["0.15", "0.24", "0.30", "0.47"],
+            "Answer" : 3
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Work out 16/32 as a decimal.",
+            "Options" : ["0.30", "0.47", "0.50", "0.58"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Simplify 16/128",
+            "Options" : ["1/8", "2/8", "1/12", "2/12"],
+            "Answer" : 0
+        },
+        {
+            "Question" : "Simplify 22/132",
+            "Options" : ["1/6", "2/6", "1/12", "2/12"],
+            "Answer" : 0
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        },
+        {
+            "Question" : "Write 12/28 as a percentage",
+            "Options" : ["12.28", "28.12", "42.8", "58.2"],
+            "Answer" : 2
+        }
+    ]
+}
+}
+function fetchJson(){
     fetch('./JSON/Quiz/ProportionalReasoning.json')
-        .then(response => response.json())
-        .then((json) => loadingJSON)
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            }
+            else {
+                throw new Error("Failed to fetch JSON")
+            }
+        })
 }
 
 function writeHTMLfromJSON(json){
+
     let html = "";
     let questionNo = 1;
 
@@ -18,10 +131,10 @@ function writeHTMLfromJSON(json){
                         <div class="quizQuestion">
                             <p class="quizQText">${questionNo}. ${object.Question}</p>
                             <div class="quizOptions">
-                                <button class="quizOption" id="question-${questionNo-1}-0" onclick="checkAnswer(${questionNo-1},0)">${object.Options[0]}</button>
-                                <button class="quizOption" id="question-${questionNo-1}-1" onclick="checkAnswer(${questionNo-1},2)">${object.Options[2]}</button>
-                                <button class="quizOption" id="question-${questionNo-1}-2" onclick="checkAnswer(${questionNo-1},3)">${object.Options[3]}</button>
-                                <button class="quizOption" id="question-${questionNo-1}-3" onclick="checkAnswer(${questionNo-1},1)">${object.Options[1]}</button>
+                                <button class="quizOption" id="question-${questionNo-1}-0" onclick="CheckAnswer(${questionNo-1},0)">${object.Options[0]}</button>
+                                <button class="quizOption" id="question-${questionNo-1}-1" onclick="CheckAnswer(${questionNo-1},2)">${object.Options[2]}</button>
+                                <button class="quizOption" id="question-${questionNo-1}-2" onclick="CheckAnswer(${questionNo-1},3)">${object.Options[3]}</button>
+                                <button class="quizOption" id="question-${questionNo-1}-3" onclick="CheckAnswer(${questionNo-1},1)">${object.Options[1]}</button>
                             </div>
                         </div>
             `;
@@ -48,7 +161,7 @@ function CorrectAnswer(question, chosenAnswer){
     document.getElementById(`question-${question}-${chosenAnswer}`).className = "quizOption quizOptionCorrect"
 }
 
-function WrongAnswer(){
+function WrongAnswer(question, chosenAnswer){
     document.getElementById(`question-${question}-${storedJSON.Questions[question].Answer}`).className = "quizOption quizOptionCorrect"
     document.getElementById(`question-${question}-${chosenAnswer}`).className = "quizOption quizOptionIncorrect"
 }
@@ -57,5 +170,5 @@ function UpdateScore(){
 
 }
 
-fetchJson(storedJSON);
+loadLocalJSON();
 totalQuestionNo = writeHTMLfromJSON(storedJSON);
